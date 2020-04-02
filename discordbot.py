5ac4fc60284530a948e8013b0d1eeba5da3b1ee5
shibaken1994 <62886462+shibaken1994@users.noreply.github.com>
2020-04-03 07:04:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,9 +12,17 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+async def on_message(message):
+    if message.content == '/cleanup':
+        if message.author.guild_permissions.administrator:
+            await message.channel.purge()
+            await message.channel.send('ハサキィ！！！')
+    
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+@client.event
 
 
 bot.run(token)
